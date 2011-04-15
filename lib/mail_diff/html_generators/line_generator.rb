18df152079f0e2f790faa03e4976973151d5afd4
Ilya Sabanin <ilya.sabanin@gmail.com>
2011-04-15 13:21:53 +0800

    Porting Eugene's change to fix weird GMail issue.

--- lib/mail_diff/html_generators/line_generator.rb
+++ lib/mail_diff/html_generators/line_generator.rb
@@ -34,18 +34,18 @@ private
   end
   
   def line_numbers
-    %Q[<td width="2%" align="right" valign="top" bgcolor="#F3F3F3" style="border-right: 1px solid #E5E5E5; padding: .3em .75em; color: #BBB;">
-<pre style="margin: 0; border: 0; padding: 0; font: 11px Monaco, 'Lucida Console', Consolas, 'Courier New', monospace;">#{ line.left_number }</pre>
+    %Q[<td width="4%" align="right" valign="top" bgcolor="#F3F3F3" style="border-right: 1px solid #E5E5E5; padding: .3em .75em; color: #BBB;">
+<pre style="width:auto; margin: 0; border: 0; padding: 0; font: 11px Monaco, 'Lucida Console', Consolas, 'Courier New', monospace;">#{ line.left_number }</pre>
 	  </td>
-	  <td width="2%" align="right" valign="top" bgcolor="#F3F3F3" style="border-right: 1px solid #E5E5E5; padding: .3em .75em; color: #BBB;">
-<pre style="margin: 0; border: 0; padding: 0; font: 11px Monaco, 'Lucida Console', Consolas, 'Courier New', monospace;">#{ line.right_number }</pre>
+	  <td width="4%" align="right" valign="top" bgcolor="#F3F3F3" style="border-right: 1px solid #E5E5E5; padding: .3em .75em; color: #BBB;">
+<pre style="width:auto; margin: 0; border: 0; padding: 0; font: 11px Monaco, 'Lucida Console', Consolas, 'Courier New', monospace;">#{ line.right_number }</pre>
     </td>]
   end
   
   def added_html
     wrapper do
       %Q[<td valign="top" style="padding: .3em 1em;">
-<pre style="margin: 0; border: 0; padding: 0; white-space: pre-wrap; font: 11px Monaco, 'Lucida Console', Consolas, 'Courier New', monospace;"><span class="gi">#{ line.format }</span></pre>
+<pre style="width:auto; margin: 0; border: 0; padding: 0; white-space: pre-wrap; font: 11px Monaco, 'Lucida Console', Consolas, 'Courier New', monospace;"><span class="gi">#{ line.format }</span></pre>
       </td>]
     end
   end
@@ -53,7 +53,7 @@ private
   def deleted_html
     wrapper do
       %Q[<td valign="top" style="padding: .3em 1em;">
-<pre style="margin: 0; border: 0; padding: 0; white-space: pre-wrap; font: 11px Monaco, 'Lucida Console', Consolas, 'Courier New', monospace;"><span class="gd">#{ line.format }</span></pre>
+<pre style="width:auto; margin: 0; border: 0; padding: 0; white-space: pre-wrap; font: 11px Monaco, 'Lucida Console', Consolas, 'Courier New', monospace;"><span class="gd">#{ line.format }</span></pre>
       </td>]
     end
   end
@@ -61,7 +61,7 @@ private
   def not_modified_html
     wrapper do
       %Q[<td valign="top" style="padding: .3em 1em;">
-<pre style="margin: 0; border: 0; padding: 0; white-space: pre-wrap; font: 11px Monaco, 'Lucida Console', Consolas, 'Courier New', monospace;">#{ line.format }</pre>
+<pre style="width:auto; margin: 0; border: 0; padding: 0; white-space: pre-wrap; font: 11px Monaco, 'Lucida Console', Consolas, 'Courier New', monospace;">#{ line.format }</pre>
 		  </td>]
 		end
   end
